f353e2f4a64f90773037e04099ea0698f6947527
alexandrusemina <alexandrusemina8@gmail.com>
2022-04-04 00:13:06 +0300

    fixed ram usage and app crashing

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -17,7 +17,4 @@ BOARD_VENDORIMAGE_PARTITION_SIZE   := 452984832
 
 # Inherit common board flags
 include device/samsung/universal7885-common/BoardConfigCommon.mk
-
-# System props
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
-
--- device.mk
+++ device.mk
@@ -9,7 +9,7 @@ $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.1-service.exynos7885
+    android.hardware.usb@1.3-service.samsung
 
 TARGET_SCREEN_HEIGHT := 2280
 TARGET_SCREEN_WIDTH := 1080
@@ -20,8 +20,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.sensor.light.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.light.xml
     
 PRODUCT_PACKAGES += \
-   fstab.exynos7904 \
-   ueventd.exynos7885.rc
-   
+   fstab.exynos7904
+
 PRODUCT_PACKAGES += \
    android.hardware.sensors@1.0-service
